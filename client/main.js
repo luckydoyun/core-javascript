75ff84e51051542837de27242dd02b1409885856
luckydoyun <doyun.jang.0128@gmail.com>
2023-01-13 13:42:13 +0900

    2023.01.13 export 되었어도 각 파일에서 import를 해야하나?

--- client/main.js
+++ client/main.js
@@ -35,7 +35,8 @@ function handler(e) {
   // let addvalue = firstvalue + secondvalue;
   console.log('firstvalue + secondvalue : ',addvalue );
 
-  clearContents('.result');
+  // clearContents('.result');
+  clearContents(result);
   insertLast(result,addvalue);
   // result.innerHTML = addvalue;
 }
@@ -45,7 +46,8 @@ function inputHandler(e) {
   let secondvalue = getInputValue(secondInput);
   let addvalue = sum(firstvalue, secondvalue);
 
-  clearContents('.result');
+  // clearContents('.result');
+  clearContents(result);
   insertLast(result,addvalue);
 }
 
