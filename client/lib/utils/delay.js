40a9d1507ab7f077d15e0384ea86159a08b64f1f
luckydoyun <doyun.jang.0128@gmail.com>
2023-01-19 15:24:49 +0900

    11

---
+++ client/lib/utils/delay.js
@@ -0,0 +1,130 @@
+// console.log('say hi');
+
+import { getNode } from "../dom/getNode.js";
+import { isNumber, isObject } from "./typeOf.js";
+
+
+
+const first = getNode('.first');
+const second = getNode('.second');
+
+function delay(callback, timeout = 1000){
+  setTimeout(callback,timeout);
+
+};
+
+// console.log(first);
+// console.log(second);
+
+/* 
+delayP().then( ()=>{
+  first.style.top = '-100px';
+  return delayP()
+}).then(()=>{
+  first.style.transform = 'rotate(360deg)';
+  second.style.transform = 'rotate(360deg)';
+  return delayP()
+}).then(()=>{
+  first.style.top = '0px';
+}).catch((err)=>{
+  console.log(`animation ${err}`);
+});
+
+function delayP(timeout = 1000){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('S U C C E S S !!');
+      // reject('F A I L E D !!');
+      
+    }, timeout);
+  })
+}
+
+delayP().then((result)=>{
+  console.log(result);
+})
+.catch((err)=>{
+  console.log(err);
+}); */
+
+// console.log( delayP() );
+// console.log( delayP() );
+
+const defaultOptions = {
+  shouldReject: false,
+  timeout: 1000,
+  successMessage: 'S U C C E S S @@',
+  failedMessage: 'F A I L E D @@',
+};
+
+
+export function delayP(
+  options = {}
+ ){
+
+  
+  // defaultOptions 객체 복사
+  let config = {...defaultOptions};
+  // console.log(options);
+
+  if(isNumber(options)){
+    config.timeout = options;
+  }
+  
+  // 객체 합성 mixin 필요 , 많이 사용하는 코드
+  if(isObject(options))
+    config = {...config, ...options};
+  
+  // config 확인 용도
+  // console.log(config);
+
+  // 객체 구조 분해 할당
+  const 
+  { shouldReject,
+    timeout,
+    successMessage,
+    failedMessage} = config;
+
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // console.log(shouldReject);
+      // console.log(timeout);
+      if(!shouldReject){
+         resolve(successMessage);
+      }
+      else{
+        reject(failedMessage);
+      }
+    }, timeout);
+  })
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 async awit                                 */
+/* -------------------------------------------------------------------------- */
+
+let result = 0;
+
+
+
+/* async function testA() {
+  let promise = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+      resolve("이해 완료!");
+    }, 1000);
+  })
+
+  let result = await promise;
+  return result;
+}
+
+console.log('즉시 찍기 result : ', result);
+
+result = await testA();
+// result = testA();
+
+setTimeout(() => {
+  console.log('3초 후에 찍기 result : ', result);
+}, 3000); */
---
+++ client/lib/utils/xhr.js
@@ -0,0 +1,278 @@
+
+/* 
+readyState
+0: uninitalized
+1: loading
+2: loaded
+3: interacitve
+4: complete
+*/
+
+import { typeError } from "../error/typeError.js";
+
+
+const data = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  }
+};
+
+// 콜백 방식
+export function xhrData({
+  url = '',
+  method = 'GET',
+  body = null,
+  headers = {
+    'Content-Type': 'applicaiton/json',
+    'Access-Control-Allow-Origin': '*',
+    'name': 'Luckydoyun',
+  },
+  onSuccess = null,
+  onFail = null,
+}) {
+  const xhr = new XMLHttpRequest();
+  // console.log('xhr 생성 후 xhr.readyState : ' , xhr.readyState);
+  // console.log(xhr);
+  // console.log('xhr 객체 console.log로 호출 후 xhr.readyState : ' , xhr.readyState);
+
+  //비동기 통신 오픈
+  xhr.open(method, url);
+
+  // Object.entries(headers).forEach(([key, value])=>{
+  //   xhr.setRequestHeader(key, value);
+  // });
+  // console.log(xhr);
+  // console.log(xhr.readyState);
+
+
+  //객체 구조 분해 할당
+
+  xhr.addEventListener('readystatechange', () => {
+    const { status, readyState, response } = xhr;
+  
+    if (status >= 200 && status < 400) {
+      if (readyState === 4) {
+        // console.log('통신 성공');
+        onSuccess(JSON.parse(response));
+      }
+    } else {
+      if(readyState ===2)
+        onFail('readyState :2 즉, 통신 실패');
+      else if(readyState ===3)
+        onFail('readyState :3 즉, 통신 실패');
+      else if(readyState ===4)
+        onFail('readyState :4 즉, 통신 실패');
+      else
+        onFail('다른 경우 통신 실패');
+      // console.error('통신 실패');
+    }
+
+  })
+
+  //서버에 요청
+  xhr.send(JSON.stringify(body));
+}
+
+// xhrData('POST', 'https://jsonplaceholder.typicode.com/users', data);
+// xhrData({
+//   url: 'https://jsonplaceholder.typicode.com/users1',
+//   onSuccess : (e)=>{
+//     console.log('onSuccess excute : ', e);
+//   },
+//   onFaild : (e)=>{
+//     console.error(`onFaild excute : ${e}`);
+//   }
+// });
+
+// console.log('xhr.open() before : ', xhr.readyState);
+// console.log('xhr.open() after : ', xhr.readyState);
+// console.log('xhr.send() after : ', xhr.readyState);
+// console.log(data);
+// console.log(JSON.stringify(data));
+
+xhrData.get = (url, onSuccess, onFail) => {
+  xhrData({
+    url,
+    onSuccess,
+    onFail
+  })
+};
+
+xhrData.post = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method: 'POST',
+    body,
+    url,
+    onSuccess,
+    onFail
+  })
+};
+
+xhrData.put = (url, body, onSuccess, onFail) => {
+  xhrData({
+    method: 'PUT',
+    url,
+    body,
+    onSuccess,
+    onFail
+  })
+};
+
+xhrData.delete = (url, onSuccess, onFail) => {
+  xhrData({
+    method: 'DELETE',
+    url,
+    onSuccess,
+    onFail
+  })
+};
+
+// console.dir(xhrData);
+// xhrData.post(
+//   'https://jsonplaceholder.typicode.com/users',
+//   data,
+//   (res) => {
+//     console.log(`post 성공 ${res}`);
+//     console.log(res);
+//   },
+//   (err) => {
+//     console.log(`post 실패 ${err}`);
+//   }
+// );
+
+// xhrData.delete(
+//   'https://jsonplaceholder.typicode.com/users/1',
+//   (res) => {
+//     console.log(`delete 성공 ${res}`);
+//     console.log(res);
+//   },
+//   (err) => {
+//     console.log(`delete 실패 ${err}`);
+//   }
+// );
+
+
+/* xhrData.get(
+  'https://jsonplaceholder.typicode.com/users',
+  (res) => {
+    console.log(`get 성공`);
+    console.log(res);
+  },
+  (err) => {
+    console.log(`get 실패 ${err}`);
+  }
+); */
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 promise API                                */
+/* -------------------------------------------------------------------------- */
+
+const defaultOptions = {
+  url: '',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'applicaiton/json',
+    'Access-Control-Allow-Origin': '*',
+    'name': 'Luckydoyun',
+  },
+  body: null
+}
+
+export function xhrPromise(options = {}){
+
+  const xhr = new XMLHttpRequest();
+
+  const {method, url, body, headers} = Object.assign({}, defaultOptions, options);
+
+  if(!url) typeError('서버와 통신할 url 인자는 반드시 필수값입니다.');
+
+  xhr.open(method, url);
+
+  xhr.send(body ? JSON.stringify(body) : null);
+
+  return new Promise((resolve, reject)=>{
+    xhr.addEventListener('readystatechange',()=>{
+      const {status, readyState, response} = xhr;
+
+      if(status >= 200 && status < 400){
+        if(readyState === 4){
+          resolve(JSON.parse(response));
+        }
+      }else{
+        reject('promise 통신 실패');
+      }
+    })
+  });
+}
+
+// xhrPromise();
+
+/* -------------------------------------------------------------------------- */
+/*                      xhrPromise, xhrCallback test code                     */
+/* -------------------------------------------------------------------------- */
+/* xhrPromise({url: 'https://jsonplaceholder.typicode.com/users'})
+.then((res)=>{
+  console.log('promise success');
+  console.log(res);
+})
+.catch((err)=>{
+  console.log('promise failed');
+  console.log(err);
+});
+
+xhrData.get(
+  // 'www.naver.com',
+  'https://jsonplaceholder.typicode.com/users',
+  (res)=>{
+    console.log('xhrData callback success');
+    console.log(res);
+  },
+  (err)=>{
+    console.log('xhrData callback failed');
+    console.log(err);
+  }
+); */
+
+xhrPromise.get = (url) => {
+  return xhrPromise({
+    url
+  })
+}
+
+
+xhrPromise.post = (url,body) => {
+  return xhrPromise({
+    url,
+    body,
+    method:'POST'
+  })
+}
+
+
+xhrPromise.put = (url,body) => {
+  return xhrPromise({
+    url,
+    body,
+    method:'PUT'
+  })
+}
+
+
+xhrPromise.delete = (url) => {
+  return xhrPromise({
+    url,
+    method:'DELETE'
+  })
+}
